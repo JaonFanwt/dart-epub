96be56ac18191861b5fc9f256914f7894d721fb2
JaonFanwt <fanwt883188@gmail.com>
2018-10-17 01:58:14 +0800

    Update root_file_path_reader.dart
    update to dart2

--- lib/src/readers/root_file_path_reader.dart
+++ lib/src/readers/root_file_path_reader.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'dart:convert';
 
+import 'package:dart2_constant/convert.dart' as convert;
 import 'package:archive/archive.dart';
 import 'package:xml/xml.dart' as xml;
 
@@ -17,7 +17,7 @@ class RootFilePathReader {
     }
 
     xml.XmlDocument containerDocument =
-    xml.parse(UTF8.decode(containerFileEntry.content));
+    xml.parse(convert.utf8.decode(containerFileEntry.content));
     xml.XmlElement packageElement = containerDocument
         .findAllElements("container",
         namespace: "urn:oasis:names:tc:opendocument:xmlns:container")
@@ -46,7 +46,7 @@ class RootFilePathReader {
     }
 
     xml.XmlDocument containerDocument =
-        xml.parse(UTF8.decode(containerFileEntry.content));
+        xml.parse(convert.utf8.decode(containerFileEntry.content));
     xml.XmlElement packageElement = containerDocument
         .findAllElements("container",
             namespace: "urn:oasis:names:tc:opendocument:xmlns:container")
